36e84a78e8266c24312c941d594093da2da5f5b2
zel <154630339+Israel-Andrade-P@users.noreply.github.com>
2026-06-05 11:54:53 -0300

    banner images added, product dispaly added to Home page

---
+++ frontend/src/utils/constant.ts
@@ -0,0 +1,5 @@
+import bannerImageOne from "../assets/sliders/living_room.png";
+import bannerImageTwo from "../assets/sliders/smart_tv.png";
+import bannerImageThree from "../assets/sliders/kidz_clothes.png";
+
+export { bannerImageOne, bannerImageTwo, bannerImageThree };
