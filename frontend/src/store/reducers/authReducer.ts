7a502dabaf5bf3f023f17cb6549e0919de82e71b
zel <154630339+Israel-Andrade-P@users.noreply.github.com>
2026-06-18 10:43:19 -0300

    added authReducer and InputField component

---
+++ frontend/src/store/reducers/authReducer.ts
@@ -0,0 +1,8 @@
+const intialState = {
+  user: null,
+  address: [],
+};
+
+export const authReducer = (state = intialState, action) => {
+  return state;
+};
--- frontend/src/store/reducers/store.ts
+++ frontend/src/store/reducers/store.ts
@@ -2,18 +2,27 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./productReducer";
 import { errorReducer } from "./errorReducer";
 import { cartReducer } from "./cartReducer";
+import { authReducer } from "./authReducer";
+
+const user = localStorage.getItem("auth")
+  ? JSON.parse(localStorage.getItem("auth"))
+  : [];
 
 const cartItems = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
-const initialState = { carts: { cart: cartItems } };
+const initialState = {
+  auth: { user: user },
+  carts: { cart: cartItems },
+};
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
     errors: errorReducer,
     carts: cartReducer,
+    auth: authReducer,
   },
   preloadedState: initialState,
 });
